8c29a144029d2ad00abe4c12d9bb6af75abbfd78
sermyu <kyhee2001@naver.com>
2025-09-09 22:39:27 +0900

    [feat]메인 색상 지정

--- app/src/main/java/com/example/plare/ui/theme/Color.kt
+++ app/src/main/java/com/example/plare/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.example.plare.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Red = Color(0xFFFF0000)
 
-val Purple40 = Color(0xFF6650a4)
+val Red100 = Color(0xFFFF6A6A)
+val Red80 = Color(0xFFFF8B8B)
+val Red50 = Color(0xFFFFCACA)
+
+val Red30 = Color(0xFFFFEBEB)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
