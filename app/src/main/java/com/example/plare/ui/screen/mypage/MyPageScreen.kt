46f24636185237d54150301b2017f32ea5ac115d
sermyu <kyhee2001@naver.com>
2025-09-11 18:17:19 +0900

    [feat]screen 생성

---
+++ app/src/main/java/com/example/plare/ui/screen/mypage/MyPageScreen.kt
@@ -0,0 +1,18 @@
+package com.example.plare.ui.screen.mypage
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyPageScreen () {
+    Scaffold() { inner ->
+        Column (modifier = Modifier.fillMaxSize().padding(inner)) {
+            Text("hi")
+        }
+    }
+}
